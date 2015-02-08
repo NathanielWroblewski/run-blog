bc6d9243e0c5b4837310805d8e4d17af568a267c
Nathaniel Wroblewski <nwroblewski@kissmetrics.com>
2015-02-07 20:45:37 -0600

    Add KISSmetrics

---
+++ public/javascripts/lib/kissmetrics.js
@@ -0,0 +1,12 @@
+var _kmq = _kmq || [];
+var _kmk = _kmk || '3bbf1a71bf11e2547e06a133986b50b2bd4e2d7b';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
